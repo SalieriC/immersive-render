99950cdf1fc59b213ca8d9dd61bc6225cb50c60e
SalieriC <salieric@t-online.de>
2022-08-18 13:03:24 +0200

    default colume setting

--- immersive-render/scripts/init.js
+++ immersive-render/scripts/init.js
@@ -4,5 +4,23 @@ Hooks.on('setup', api.registerFunctions)
 
 Hooks.on(`ready`, () => {
     // Ready stuff
+    register_settings();
     console.log('Immersive Render (IR) | Ready');
 })
+
+export function register_settings() {
+    // Default Volume
+    game.settings.register('ir', 'defaultVolume', {
+        name: game.i18n.localize("IR.defaultVolumeName"),
+        hint: game.i18n.localize("IR.defaultVolumeHint"),
+        type: Number,
+        range: {
+            min: 0,
+            max: 1,
+            step: 0.1
+        },
+        default: 1,
+        scope: 'user',
+        config: true,
+    });
+}

22361a4706470872916ecce91cca3db1187e09e0
SalieriC <salieric@t-online.de>
2022-08-22 16:49:44 +0200

    generic system support

--- immersive-render/scripts/init.js
+++ immersive-render/scripts/init.js
@@ -1,11 +1,15 @@
 import { api } from './api.js'
 import { register_settings } from './settings.js'
 import { register_context,register_folder_context } from './contextual_options.js'
+import { entity_hooks } from './entity_hooks.js'
+import { set_vars_by_system } from './system_variables.js'
 
 Hooks.on('setup', api.registerFunctions)
 
 Hooks.on(`ready`, () => {
     // Ready stuff
+    let { renderJournalSheet, closeJournalSheet, renderActorSheet, closeActorSheet, renderItemSheet, closeItemSheet, createChatMessage } = set_vars_by_system()
+    entity_hooks(renderJournalSheet, closeJournalSheet, renderActorSheet, closeActorSheet, renderItemSheet, closeItemSheet, createChatMessage)
     register_settings()
     console.log('Immersive Render (IR) | Ready')
     if (game.user?.isGM && game.settings.get('immersive-render', 'confirm00') === false) {
@@ -66,66 +70,3 @@ Hooks.on("getItemDirectoryFolderContext", (html, entryOptions) => {
 Hooks.on("getJournalDirectoryFolderContext", (html, entryOptions) => {
     register_folder_context(entryOptions)
 });
-
-// Hooks for playing SFX:
-Hooks.on('renderJournalSheet', async (entitySheet, _, __) => {
-    let entity = entitySheet.object
-    let { currOpenPath, currClosePath, currVolume } = await ir.get_flags(entity)
-    if (currOpenPath) {
-        ir.play_random_audio(currOpenPath, currVolume, false)
-    }
-})
-Hooks.on('closeJournalSheet', async (entitySheet, _, __) => {
-    let entity = entitySheet.object
-    let { currOpenPath, currClosePath, currVolume } = await ir.get_flags(entity)
-    if (currClosePath) {
-        ir.play_random_audio(currClosePath, currVolume, false)
-    }
-})
-
-Hooks.on('renderCharacterSheet', async (entitySheet) => {
-    let entity = entitySheet.object
-    let { currOpenPath, currClosePath, currVolume } = await ir.get_flags(entity)
-    if (currOpenPath) {
-        ir.play_random_audio(currOpenPath, currVolume, false)
-    }
-})
-Hooks.on('closeCharacterSheet', async (entitySheet) => {
-    let entity = entitySheet.object
-    let { currOpenPath, currClosePath, currVolume } = await ir.get_flags(entity)
-    if (currClosePath) {
-        ir.play_random_audio(currClosePath, currVolume, false)
-    }
-})
-
-Hooks.on('renderItemSheet', async (entitySheet) => {
-    let entity = entitySheet.object
-    let { currOpenPath, currClosePath, currVolume } = await ir.get_flags(entity)
-    if (currOpenPath) {
-        ir.play_random_audio(currOpenPath, currVolume, false)
-    }
-})
-Hooks.on('closeItemSheet', async (entitySheet) => {
-    let entity = entitySheet.object
-    let { currOpenPath, currClosePath, currVolume } = await ir.get_flags(entity)
-    if (currClosePath) {
-        ir.play_random_audio(currClosePath, currVolume, false)
-    }
-})
-
-let timedOut = false
-Hooks.on('createChatMessage', async (message) => {
-    let chatMessageSfx = game.settings.get('immersive-render', 'chatMessageSfx')
-    if (chatMessageSfx && timedOut === false && message.data.whisper.length <= 0 && game.userId != message.data.user) {
-        ir.play_audio(chatMessageSfx, -1, false)
-        timedOut = true
-        await wait(game.settings.get('immersive-render', 'timeOut'))
-        timedOut = false
-    }
-})
-
-async function wait(ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
-}
